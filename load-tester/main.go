50832be654413a6728ff63f2f8ecb26d1267e8d7
Brian Yin <brianyin256@gmail.com>
2026-07-07 22:20:40 -0400

    update worker agent to take duration and rps

--- load-tester/main.go
+++ load-tester/main.go
@@ -1,53 +1,120 @@
 package main 
 
 import (
+	"context"
 	"flag"
 	"fmt"
-	"log"
-//	"io"
 	"net/http"
+	"sync"
 	"time"
 )
 
-func main() {
+type Result struct {
+	latency time.Duration
+	err     error
+}
 
+func main() {
+	
 	port := flag.Int("port", 8080, "Port of the load balancer")
-	req := flag.Int("req", 100, "Number of requests sent to the load balancer")
+	duration := flag.Duration("duration", 30*time.Second, "how long to run the test")
+	reqPerSecond := flag.Int("rps", 100, "Number of requests per second")
+	numWorkers := flag.Int("workers", 10, "Number of workers sending requests")
 
 	flag.Parse()
 
 	var latencies []float32
-	length := *req
 
+	dur := *duration
+	rps := *reqPerSecond
+	workers := *numWorkers
 	url := fmt.Sprintf("http://localhost:%d", *port)
 
-	for i := 0; i < length; i++ {
+	tokens := make(chan struct{}, rps)
+	interval := time.Second / time.Duration(rps)
 
-		start := time.Now()
-		resp, err := http.Get(url)
+	overallStart := time.Now()
 
-		if err != nil { log.Fatal(err) }
+	ctx, cancel := context.WithTimeout(context.Background(), dur)
+	defer cancel()
 
-		//bodyBytes, err := io.ReadAll(resp.Body)
+	// goroutine for rate limiting
+	go func() {
+		ticker := time.NewTicker(interval)
+		defer ticker.Stop()
+		defer close(tokens)
 
-		//if err != nil { log.Fatal(err) }
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-ticker.C:
+				select {
+				case tokens <- struct{}{}:
+				default:
+				}
+			}
+		}
+	}()
 
-		//content := string(bodyBytes)
+	var wg sync.WaitGroup
 
-		//fmt.Println("content:", content)
-		latency := time.Since(start)
-		fmt.Println("ping response time:", latency)
+	// eventually needs a separate goroutine to drain this or else it could cause the script to block
+	results := make(chan Result, rps * int(dur.Seconds()))
 
-		latencies = append(latencies, float32(latency.Seconds() * 1000))
+	// worker goroutines 
+	for i := 0; i < workers; i++ {
 
-		resp.Body.Close()
+		wg.Add(1)
+		
+		go func() {
+			defer wg.Done()
+
+			for range tokens {
+				start := time.Now()
+				resp, err := http.Get(url)
+
+				if err != nil { 
+					results <- Result{err: err}
+					continue
+				}
+
+				latency := time.Since(start)
+				fmt.Println("ping response time:", latency)
+
+				results <- Result{latency: latency}
+
+				resp.Body.Close()
+			}
+		} ()
+	}
+
+	wg.Wait()
+
+	elapsed := time.Since(overallStart)
+
+	close(results)
+
+	for r := range results {
+		if r.err != nil {
+			fmt.Println("error:", r.err)
+			continue
+		}
+
+		latencies = append(latencies, float32(r.latency.Seconds() * 1000))
 	}
 
 	var sum float32
+
+	if len(latencies) == 0 {
+		fmt.Println("no successful requests")
+		return
+	}
+
 	minScore := latencies[0]
 	maxScore := latencies[0]
 
-	for i := 0; i < length; i++ {
+	for i := 0; i < len(latencies); i++ {
 		sum += latencies[i]
 
 		if latencies[i] < minScore {
@@ -59,9 +126,14 @@ func main() {
 		}
 	}
 
-	fmt.Println("-----------------------------------------")
-	fmt.Println("pings:", length)
-	fmt.Println("avg:", sum / float32(length), "ms")
+	fmt.Println("--------------- { config } ---------------")
+	fmt.Println("workers:", workers)
+	fmt.Println("rps:", rps)
+	fmt.Println("duration:", elapsed.Seconds(), "s")
+
+	fmt.Println("--------------- { results } ---------------")
+	fmt.Println("pings:", len(latencies))
+	fmt.Println("avg:", sum / float32(len(latencies)), "ms")
 	fmt.Println("max:", maxScore, "ms")
 	fmt.Println("min:", minScore, "ms")
 	
